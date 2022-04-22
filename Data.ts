040df7de3806fcea8938e1d7a7e5a16d537b9627
Ivan Garcia <oi.garcia@uniandes.edu.co>
2022-04-21 22:50:08 -0500

    a

--- Data.ts
+++ Data.ts
@@ -1,4 +1,6 @@
-export const series = [
+import { Serie } from './scripts/Serie.js';
+
+export const seriesData = [
     new Serie (1,"Breaking Bad","AMC", 5,"Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer" ,
     "https://www.amc.com/shows/breaking-bad","https://i.imgur.com/GGje0vc.jpg") ,
     
