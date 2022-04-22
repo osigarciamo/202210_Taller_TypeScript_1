064b2027393e7c7ff6b22b9f90f6b5bf5928b7e7
Oscar Ivan Garcia Montañez <oscarivangarciamontanez@MacBook-Air-de-Oscar.local>
2022-04-21 23:16:58 -0500

    Final

--- main.ts
+++ main.ts
@@ -1,24 +1,22 @@
-import { Serie } from "./Serie";
-import { dataSeries } from "./Data";
+import { Serie } from "./Serie.js";
+import { dataSeries } from "./Data.js";
 
-const seriesTbody: HTMLElement = document.getElementById('series')!;
-const avgSeasonNumber: HTMLElement= document.getElementById('avg-seasons')!;
+const seriesTbody: HTMLElement = document.getElementById('seriesTV')!; // Nodo tbody que tiene el id="seriesTV"
+const avgSeasonNumber: HTMLElement= document.getElementById('average')!;
 
 renderSeriesInTable(dataSeries);
 avgSeasonNumber.innerHTML = `${seasonsAverage(dataSeries)}`
 
+/* Lee los datos de un arreglo de objetos series y finalmente lo despliega en la tabla de series actuales (DOM). */
 function renderSeriesInTable(series: Serie[]): void {
-  console.log('Desplegando programas de televisión y series');
-  series.forEach((serie) => {
-    let trElement = document.createElement("tr");
-    trElement.innerHTML = `<td>${serie.id}</td>
-                           <td>${serie.name}</td>
-                           <td>${serie.channel}</td>
-                           <td>${serie.seasons}</td>`;
-    seriesTbody.appendChild(trElement);
+    console.log('Desplegando programas de televisión y series');
+    series.forEach(serie => {
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td>${serie.id}</td>`;
+        seriesTbody.appendChild(trElement);
   });
 }
-
+/* Calcula el promedio de las temporadas. */
 function seasonsAverage (series : Serie[]): number {
     console.log('Determinando el promedio de las series');
     let totalSeasons: number=0;
